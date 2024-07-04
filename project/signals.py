b6b79bef85ad9d3106afe60f7b581bbcc4eab44f
hamidba <balalzadehhamid79@gmail.com>
2024-07-04 08:44:15 +0330

    fix csv

--- project/signals.py
+++ project/signals.py
@@ -8,34 +8,33 @@ from project.models import Block, MainChart, ChildChart
 @receiver(post_save, sender=Block, dispatch_uid="generate_charts")
 def generate_chrats(sender, instance, created, **kwargs):
     """Generate Charts"""
-    if created:
-        with open(instance.main_csv_data.path, "r", encoding="utf-8") as main_csv_file:
-            reader = csv.DictReader(main_csv_file)
-            for row in reader:
-                main = MainChart.objects.create(
-                    index=row["index"],
-                    label=row["label"],
-                    percent=int(row["percent"]),
-                    color=row["color"],
-                    block=instance,
-                )
-                main.save()
+    with open(instance.main_csv_data.path, "r", encoding="utf-8") as main_csv_file:
+        reader = csv.DictReader(main_csv_file)
+        for row in reader:
+            main = MainChart.objects.create(
+                index=row["index"],
+                label=row["label"],
+                percent=int(row["percent"]),
+                color=row["color"],
+                block=instance,
+            )
+            main.save()
 
-                with open(instance.child_csv_file.path, "r", encoding="utf-8") as child_csv_file:
-                    child_reader = csv.DictReader(child_csv_file)
-                    for row in child_reader:
-                        main_id = 0
-                        try:
-                            main_id = int(row["main_id"])
-                        except:
-                            pass
+            with open(instance.child_csv_file.path, "r", encoding="utf-8") as child_csv_file:
+                child_reader = csv.DictReader(child_csv_file)
+                for row in child_reader:
+                    main_id = 0
+                    try:
+                        main_id = int(row["main_id"])
+                    except:
+                        pass
 
-                        if main_id == int(main.index):
-                            child = ChildChart.objects.create(
-                                index=row["index"],
-                                label=row["label"],
-                                percent=int(row["percent"]),
-                                color=row["color"],
-                                main=main,
-                            )
-                            child.save()
+                    if main_id == int(main.index):
+                        child = ChildChart.objects.create(
+                            index=row["index"],
+                            label=row["label"],
+                            percent=int(row["percent"]),
+                            color=row["color"],
+                            main=main,
+                        )
+                        child.save()
