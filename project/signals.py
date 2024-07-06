1bb9f84babff494130a0e58a388516a8e40e4b1c
hamidba <balalzadehhamid79@gmail.com>
2024-07-06 10:55:51 +0330

    remvove old charts

--- project/signals.py
+++ project/signals.py
@@ -11,6 +11,11 @@ def generate_chrats(sender, instance, created, **kwargs):
     if instance.type_csv_file and instance.main_csv_file and instance.child_csv_file:
         with open(instance.type_csv_file.path, "r", encoding="utf-8") as type_csv_file:
             reader = csv.DictReader(type_csv_file)
+            
+            old_types = ChartType.objects.filter(block=instance)
+            for old_type in old_types:
+                old_type.delete()
+                
             for row in reader:
                 type = ChartType.objects.create(
                     index=row["index"],
@@ -20,6 +25,11 @@ def generate_chrats(sender, instance, created, **kwargs):
                 type.save()
                 with open(instance.main_csv_file.path, "r", encoding="utf-8") as main_csv_file:
                     reader = csv.DictReader(main_csv_file)
+                    
+                    old_mains = MainChart.objects.filter(type=type)
+                    for old_main in old_mains:
+                        old_main.delete()
+                        
                     for row in reader:
                         type_id = 0
                         try:
@@ -39,6 +49,11 @@ def generate_chrats(sender, instance, created, **kwargs):
 
                             with open(instance.child_csv_file.path, "r", encoding="utf-8") as child_csv_file:
                                 child_reader = csv.DictReader(child_csv_file)
+                                
+                                old_childs = ChildChart.objects.filter(main=main)
+                                for old_child in old_childs:
+                                    old_child.delete()
+                                
                                 for row in child_reader:
                                     main_id = 0
                                     try:
